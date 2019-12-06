3d8dbe3bfe6426873dc42ba16294391eae679bf8
Rogier Morsink <rogier.morsink@inology.nl>
2019-12-06 12:11:50 +0100

    dag 5 deel 2, finally

--- advent_2019/src/main/java/days/Five/Five_partTwo.java
+++ advent_2019/src/main/java/days/Five/Five_partTwo.java
@@ -7,10 +7,18 @@ import static java.lang.System.exit;
  * @author Rogier
  */
 public final class Five_partTwo {
+    static int[] startArray;
+
     public static void main(String[] args) {
         ArrayCreator creator = new ArrayCreator();
-        int[] startArray = creator.createIntArrayFromfile("src/main/java/days/Five/input", ",");
+        startArray = creator.createIntArrayFromfile("src/main/java/days/Five/input", ",");
+        //3970568 is het niet
 
+        //answer might be 7704130
+        //answer might be 7704130
+//int[] startArray={3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+//        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+//        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99};
 //        int[] startArray= {3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99};
 //        int[] startArray= {3,9,8,9,10,9,4,9,99,-1,8}; // if input is 8, result is 1, orther it is 0
 //        int[] startArray= {3,3,1108,-1,8,3,4,3,99}; // if input is 8, result is 1, orther it is 0
@@ -22,112 +30,76 @@ public final class Five_partTwo {
 //        int[] startArray= {1002,4,3,4,33};
 //        int[] startArray= {1101,100,-1,4,0};
 //
-//        startArray[1]=12;
-//        startArray[2]=2;
 
 
         //answer 3970568 is to low
-        int increase;
+        int increase=-1000;
         for (int i = 0; i < startArray.length; i += increase) {
-            System.out.println("start op loop---");
-            System.out.println("i is " + i);
             int instructionsOptcode = startArray[i];
-            int whatToDo = instructionsOptcode % 10;
+            int whatToDo = instructionsOptcode % 100;
 
-            increase = 4;
+//            increase = 4;
             System.out.println("instructionsOptcode is " + instructionsOptcode);
             System.out.println("WhatToDo is " + whatToDo);
 
 
             int modeOne = ((instructionsOptcode - whatToDo) % 1000) / 100;
-            int modeTwo = ((instructionsOptcode - whatToDo - modeOne * 100) % 10000) / 1000;
-            int modeThree = ((instructionsOptcode - whatToDo - modeOne * 100 - modeTwo * 1000) % 100000) / 10000;
+            int modeTwo = ((instructionsOptcode - whatToDo - (modeOne * 100)) % 10000) / 1000;
 
-            System.out.println("instructionsOptcode = is " + instructionsOptcode);
-            System.out.println("modes:  " + modeOne + " " + modeTwo + " " + modeThree);
-            if (whatToDo== 99 ||whatToDo==9) {
-                exit(0);
-            }
-            int one = startArray[i + 1];
 
-            int two = startArray[i + 2];
-            int three = 0;
-            if (whatToDo == 4 || whatToDo == 5 || whatToDo == 6) {
-
-            } else {
-                three = startArray[i + 3];
-            }
-            System.out.println("First value to use = " + one);
-            System.out.println("Second value to use = " + two);
-            System.out.println("Third value to use = " + three);
-            int valueOne;
-            int valueTwo = 0;
-            int valueThree = 0;
-
-            if (modeOne == 1) {
-                valueOne = one;
-            } else {
-                valueOne = startArray[one];
-            }
-            System.out.println("First value results in " + valueOne);
-            if (whatToDo == 1 || whatToDo == 2 || whatToDo == 5 || whatToDo == 6 || whatToDo == 7 || whatToDo == 8) {
-                if (modeTwo == 1) {
-                    valueTwo = two;
-                } else {
-                    valueTwo = startArray[two];
-                }
-            }
-            if (whatToDo == 1 || whatToDo == 2 || whatToDo == 7 || whatToDo == 8) {
-                if (modeThree == 1) {
-                    valueThree = three;
-                } else {
-                    valueThree = startArray[three];
-                }
-                System.out.println("Second value results in " + valueTwo);
-                System.out.println("Third value results in " + valueThree);
-            }
 
             switch (whatToDo) {
 
                 case 1: {
+                    int one = getOne(i);
+                    int two = getTwo(i);
+                    int three = getThree(i);
+                    int valueOne = calculateValue(one, modeOne);
+                    int valueTwo = calculateValue(two, modeTwo);
                     System.out.println("adding " + (valueOne + valueTwo) + " on location  " + three);
                     startArray[three] = valueOne + valueTwo;
+                    increase = 4;
                     break;
                 }
 
                 case 2: {
+                    int one = getOne(i);
+                    int two = getTwo(i);
+                    int three = getThree(i);
+                    int valueOne = calculateValue(one, modeOne);
+                    int valueTwo = calculateValue(two, modeTwo);
+
                     System.out.println("multiplying storing " + (valueOne * valueTwo) + " on location " + three);
                     startArray[three] = valueOne * valueTwo;
+                    increase = 4;
                     break;
                 }
                 case 3: {
+                    int one = getOne(i);
                     System.out.println("asks for input");
                     startArray[one] = 5;//never writes in imediate mote
                     System.out.println("store 5 on location " + one);
-//                    System.out.println("store ZERRRO on location " + one);
                     increase = 2;
+
                     break;
                 }
                 case 4: {
+                    int one = getOne(i);
+                    int valueOne = calculateValue(one, modeOne);
+
                     System.out.println("output---------------------------------------------------------");
                     System.out.println(valueOne);
-//                    System.out.println(one);
-//                    System.out.println(startArray[one]);
-//                    System.out.println(startArray[valueOne]);
                     increase = 2;
                     break;
                 }
                 case 5: {
                     //Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-
-
-//                    if (startArray[valueOne]!=0){
+                    int one = getOne(i);
+                    int two = getTwo(i);
+                    int valueOne = calculateValue(one, modeOne);
+                    int valueTwo = calculateValue(two, modeTwo);
                     if (valueOne != 0) {
-//                        i=startArray[two];
-//                        i=startArray[valueTwo];
                         i = valueTwo;//denk dat dit hem moet zijn
-//                        i=two;
-
                         increase = 0;
                     } else {
                         increase = 3;
@@ -135,13 +107,12 @@ public final class Five_partTwo {
                     break;
                 }
                 case 6: {
-//                    if (startArray[valueOne]==0){
+                    int one = getOne(i);
+                    int two = getTwo(i);
+                    int valueOne = calculateValue(one, modeOne);
+                    int valueTwo = calculateValue(two, modeTwo);
                     if (valueOne == 0) {
-
-//                        i=startArray[two];
-//                        i=startArray[valueTwo];
                         i = valueTwo;//denk dat dit hem moet zijn
-//                        i=two;
                         increase = 0;
                     } else {
                         increase = 3;
@@ -150,22 +121,35 @@ public final class Five_partTwo {
                     break;
                 }
                 case 7: {
+                    int one = getOne(i);
+                    int two = getTwo(i);
+                    int three = getThree(i);
+                    int valueOne = calculateValue(one, modeOne);
+                    int valueTwo = calculateValue(two, modeTwo);
+
+
                     int upDateValue = 0;
-                    if (startArray[valueOne] < startArray[valueTwo]) {
+                    if (valueOne < valueTwo) {
                         upDateValue = 1;
                     }
                     startArray[three] = upDateValue;
+                    increase = 4;
                     break;
                 }
                 case 8: {
+                    int one = getOne(i);
+                    int two = getTwo(i);
+                    int three = getThree(i);
+                    int valueOne = calculateValue(one, modeOne);
+                    int valueTwo = calculateValue(two, modeTwo);
                     int upDateValue = 0;
-                    if (startArray[valueOne] == startArray[valueTwo]) {
+                    if (valueOne == valueTwo) {
                         upDateValue = 1;
                     }
                     startArray[three] = upDateValue;
+                    increase = 4;
                     break;
                 }
-                case 9:
                 case 99: {
                     System.out.println("99 is bereikt");
                     System.out.println(startArray[0]);
@@ -180,8 +164,31 @@ public final class Five_partTwo {
                     break;
             }
 
-            System.out.println("increase is " + increase);
-            System.out.println("End of loop---");
         }
     }
+
+    static int getOne(int i) {
+        return startArray[i + 1];
+    }
+
+    static int getTwo(int i) {
+        return startArray[i + 2];
+    }
+
+    static int getThree(int i) {
+        return startArray[i + 3];
+    }
+
+    static int calculateValue(int waarde, int modus) {
+        if (modus == 1) {
+            return waarde;
+        }
+        if (modus == 0) {
+            return startArray[waarde];
+        }
+        System.out.println("kan niet++++++++++++++++++++++++++++++");
+        exit(999);
+        return 2;
+    }
+
 }
---
+++ advent_2019/src/main/java/days/Five/SecondPartStarter.java
@@ -0,0 +1,17 @@
+package days.Five;
+
+import days.Three.ArrayCreator;
+
+/**
+ * @author Rogier
+ */
+public final class SecondPartStarter {
+    public static void main(String[] args) {
+
+        ArrayCreator creator = new ArrayCreator();
+        int[] startArray = creator.createIntArrayFromfile("src/main/java/days/Five/input", ",");
+
+
+    }
+
+}
