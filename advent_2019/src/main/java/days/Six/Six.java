2ad38413329cbc1f0b5498bd947458d72a9ff76d
Rogier Morsink <rogier.morsink@inology.nl>
2019-12-06 15:10:14 +0100

    dag 6 deel 2

--- advent_2019/src/main/java/days/Six/Six.java
+++ advent_2019/src/main/java/days/Six/Six.java
@@ -1,6 +1,8 @@
 package days.Six;
 
 import days.Three.ArrayCreator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Six {
 
@@ -9,11 +11,10 @@ public class Six {
 
         String[] instap = ding.createFromFile("src/main/java/days/Six/input", ",");
         String[][] kaart = new String[instap.length][2];
-        String[][] diepteMap = new String[instap.length+1][2];
+        String[][] diepteMap = new String[instap.length + 1][2];
         for (int i = 0; i < instap.length; i++) {
             kaart[i] = instap[i].split("&");
         }
-
 
 
         int gevonden = 0;
@@ -22,36 +23,69 @@ public class Six {
         gevonden++;
 
 
-        for (int i = 0; (i < gevonden && i<kaart.length); i++) {
+        for (int i = 0; (i < gevonden && i < kaart.length); i++) {
             String teZoeken = diepteMap[i][0];
             int level = Integer.parseInt(diepteMap[i][1]) + 1;
 
-            for (int j = 0; j < kaart.length; j++){
-                if(kaart[j][0].equals(teZoeken)){
-                    diepteMap[gevonden][0]=kaart[j][1];
-                    diepteMap[gevonden][1]= Integer.toString(level);
+            for (int j = 0; j < kaart.length; j++) {
+                if (kaart[j][0].equals(teZoeken)) {
+                    diepteMap[gevonden][0] = kaart[j][1];
+                    diepteMap[gevonden][1] = Integer.toString(level);
                     gevonden++;
                 }
             }
         }
 
+
+        int totaal = 0;
         for (int i = 0; i < diepteMap.length; i++) {
-            System.out.print(diepteMap[i][0]);
-            System.out.print("      ");
-            System.out.println(diepteMap[i][1]);
-
-
-        }
-
-
-
-
-        int totaal=0;
-        for (int i = 0; i < diepteMap.length; i++) {
-            System.out.println(diepteMap[i][0]+ "   "+ diepteMap[i][1]);
-        totaal =totaal + Integer.parseInt(diepteMap[i][1]);
+            totaal = totaal + Integer.parseInt(diepteMap[i][1]);
         }
         System.out.println(totaal);
+
+
+        boolean zoekenwenog = true;
+        String nuZoekenWeDit = "YOU";
+        List <String> youList =new ArrayList();
+        while (zoekenwenog) {
+            for (int i = 0; i < kaart.length; i++) {
+                if (nuZoekenWeDit.equals(kaart[i][1])) {
+
+                    nuZoekenWeDit = kaart[i][0];
+                    youList.add(nuZoekenWeDit);
+                    if (nuZoekenWeDit.equals("COM")) {
+                        zoekenwenog = false;
+                    }
+                }
+            }
+        }
+
+
+        zoekenwenog = true;
+        nuZoekenWeDit = "SAN";
+        List <String> sanList =new ArrayList();
+        while (zoekenwenog) {
+            for (int i = 0; i < kaart.length; i++) {
+                if (nuZoekenWeDit.equals(kaart[i][1])) {
+                    nuZoekenWeDit = kaart[i][0];
+                    sanList.add(nuZoekenWeDit);
+                    if (nuZoekenWeDit.equals("COM")) {
+                        zoekenwenog = false;
+                    }
+                }
+            }
+        }
+
+
+        for (int i=0; i<youList.size() && i< sanList.size(); i++){
+            if (youList.get(youList.size()-i-1).equals(sanList.get(sanList.size()-i-1))){
+            }     else {
+                int anser= (sanList.size()  -i + (youList.size() -i));
+                System.out.println(anser);
+                break;
+            }
+        }
+
 
 //        answer = 247089
     }
